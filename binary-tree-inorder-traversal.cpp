f4fd43d78c4a32088987d45bf9e3d0ec8ef9a02e
fashengli <fashengli@realnetworks.com>
2015-01-26 10:47:18 +0800

    add morris method for inorder traversal.

--- binary-tree-inorder-traversal.cpp
+++ binary-tree-inorder-traversal.cpp
@@ -9,7 +9,7 @@
  */
 class Solution {
 public:
-#define M2
+#define M3
 #ifdef M1 // 递归版本
     vector<int> inorderTraversal(TreeNode *root) {
         vector<int> result;
@@ -24,6 +24,7 @@ public:
     }
 #endif
 #ifdef M2 // 迭代版本
+// O(n) time, O(n) space
     vector<int> inorderTraversal(TreeNode *root) {
         vector<int> result;
         if (!root) return result;
@@ -43,6 +44,40 @@ public:
             }
         }
         
+        return result;
+    }
+#endif
+#ifdef M3
+// Morris traversal, O(n) time, O(1) space
+// please refer: http://www.cnblogs.com/AnnieKim/archive/2013/06/15/morristraversal.html
+    vector<int> inorderTraversal(TreeNode *root) {
+        vector<int> result;
+
+        TreeNode *cur = root, *prev = NULL;
+        while(cur) {
+            if (cur->left == NULL) {
+                result.push_back(cur->val);
+                cur = cur->right;
+            }
+            else {
+                // 在当前结点的左子树中找到它在中序遍历中的前驱结点
+                prev = cur->left;
+                while(prev->right != NULL && prev->right != cur) {
+                    prev = prev->right;
+                }
+
+                if (prev->right == NULL) {  // 建立联系，将前驱结点的右孩子指向当前结点
+                    prev->right = cur;
+                    cur = cur->left;
+                }
+                else {
+                    prev->right = NULL; // 当前结点的左子树已经遍历过了，断开其前驱结点的右结点（恢复二叉树的形状）
+                    result.push_back(cur->val);
+                    cur = cur->right;
+                }
+            }
+        }
+
         return result;
     }
 #endif
