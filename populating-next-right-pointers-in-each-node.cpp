34548ed1db546ffe117133c35e322caf1bd78218
fashengli <fashengli@realnetworks.com>
2015-01-27 10:20:11 +0800

    solution for: populating next right pointers in each node.

---
+++ populating-next-right-pointers-in-each-node.cpp
@@ -0,0 +1,84 @@
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+#define M2
+#ifdef M1
+/*
+思路：采用先序遍历的迭代版本，用一个栈辅助 （不知道这算不算不符合题意呢，因为这是O(n) space。
+如果一定要O(1) space， 只能morris方法。）
+在遍历每个结点p的时候，让p的左孩子指向p的右孩子，而p的右孩子则指向p的next的左孩子(如果p的next不为空)。
+*/
+    void connect(TreeLinkNode *root) {
+        if (!root) return;
+        
+        stack<TreeLinkNode*> s;
+        TreeLinkNode *p;
+        
+        s.push(root);
+        while(!s.empty()) {
+            p = s.top();
+            s.pop();
+            
+            if (p->left && p->right) {
+                p->left->next = p->right; // left child points to right child
+                if (p->next) {
+                    p->right->next = p->next->left; // right child points to its parent's next's left
+                }
+                
+                s.push(p->right);
+                s.push(p->left);
+            }
+        }
+    }
+#endif
+#ifdef M2
+// morris 中序遍历的方法， O(n) time, O(1) space
+    void connect(TreeLinkNode *root) {
+        TreeLinkNode *cur = root, *prev = NULL;
+        
+        while (cur) {
+            if (cur->left == NULL) {
+                // 此处省略访问当前结点的代码
+                cur = cur->right; // 注意在morris方法中，叶子结点的right是用来指向中序遍历的后续结点的，所以此处cur->right肯定不为空。
+            }
+            else {
+                //下面这段代码不能放在访问当前结点的位置，跟一遍题目中的测试用例即明白了。
+                // 放在这能保证每个结点的左孩子尽早指向它的右孩子，这样左孩子的子树就能利用这个信息了。
+                // 例外，放在这会导致这段代码执行两遍，虽然效率上不会右太大损失，但还是加上一个if判断。
+                //--------------------------------------
+                if(cur->left->next == NULL) {
+                    cur->left->next = cur->right;
+                    if (cur->next) {
+                        cur->right->next = cur->next->left;
+                    }
+                }
+                //--------------------------------------
+                
+                prev = cur->left;
+                while(prev->right && prev->right != cur) {
+                    prev = prev->right;
+                }
+                
+                if (prev->right == NULL) {
+                    prev->right = cur;
+                    cur = cur->left;
+                }
+                else {
+                    prev->right = NULL;
+                    // 此处省略访问当前结点的代码
+                    cur = cur->right;
+                }
+                
+                
+            }
+        }
+    }
+#endif
+};
