e70a8cab22517631ec2020c87da62556e6e27767
fashengli <fashengli@realnetworks.com>
2015-01-28 14:26:24 +0800

    add 'heap' method for merge k sorted list.

--- merge-k-sorted-lists.cpp
+++ merge-k-sorted-lists.cpp
@@ -8,7 +8,7 @@
  */
 class Solution {
 public:
-#define M2
+#define M3
  
     ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
         ListNode dummy(-1);
@@ -31,7 +31,17 @@ public:
     }
     
 #ifdef M1
-// O(n1+n2+n3...) time 超时了
+/* 超时了!!!
+时间复杂度分析：
+为了便于分析，我们假设这K个list，每个list的大小都为n。
+这里会进行K-1次合并，每次合并的时间分别是：
+n +n = 2n,
+2n+n = 3n,
+3n+n = 4n,
+...
+(K-1)n +n = K*n
+所以总的时间复杂度为：2n+3n+4n+...+k*n = O(nk * k)
+*/
     ListNode *mergeKLists(vector<ListNode *> &lists) {
         if (lists.empty()) return NULL;
         
@@ -45,6 +55,15 @@ public:
 #endif
  
 #ifdef M2 // 分治法
+/*
+同样假设这K个list，每个list的大小都为n。
+T(k) = 2T(k/2) + nk
+     = 4T(k/4) + 2nk
+     = 8T(k/8) + 3nk
+     = ...
+     = kT(1) + logk * nk
+所以时间复杂度为O(nk * logk)
+*/
     typedef vector<ListNode*>::iterator Iterator;
     ListNode *mergeKLists(vector<ListNode *> &lists) {
         return mergeKLists(lists.begin(), lists.end());
@@ -60,6 +79,45 @@ public:
         ListNode *left = mergeKLists(first, first+d/2);
         ListNode *right = mergeKLists(first+d/2, last);
         return mergeTwoLists(left, right);
+    }
+#endif
+#ifdef M3 // 使用堆的方法
+/*
+http://blog.csdn.net/linhuanmars/article/details/19899259
+维护一个K个元素的最小堆，一开始把每个list的第一个（也是最小的）元素加入堆中，
+然后从堆中取出这K个元素中的最小元素，连接到最终链表中，并将该元的下一个元素加入堆中。
+该算法对每个元素要读取一次，即是k*n次，然后每次读取元素要把新元素插入堆中要logk的复杂度，
+所以总时间复杂度是O(nklogk)。空间复杂度是堆的大小，即为O(k)。
+*/
+    class ListNodeComparison {
+        public:
+        bool operator()(const ListNode *lh, const ListNode *rh) {
+            return lh->val > rh->val;  // > 是最小堆， < 最大堆
+        }
+    };
+    ListNode *mergeKLists(vector<ListNode *> &lists) {
+        if (lists.empty()) return NULL;
+
+        std::priority_queue<ListNode*, vector<ListNode*>, ListNodeComparison> q;
+        ListNode dummy(-1);
+        ListNode *prev = &dummy;
+        for(int i=0;i<lists.size(); ++i) {
+            if (lists[i]) {
+                q.push(lists[i]);
+            }
+        }
+
+        while (!q.empty()) {
+            ListNode *p = q.top();
+            q.pop();
+            prev->next = p;
+            prev = prev->next;
+            if (p->next) {
+                q.push(p->next);
+            }
+        }
+
+        return dummy.next;
     }
 #endif
 };
