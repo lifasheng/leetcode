d592568c0f94220d388f25e1151a8c6d80818b63
fashengli <fashengli@realnetworks.com>
2015-02-13 14:24:43 +0800

    solution for: combination sum ii.

---
+++ combination-sum-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+// 这道题主要是注意重复元素的处理。
+// test case:
+// [1 1]   target = 1, output [1]
+// [1 1 2] target = 3, output = [1 2]
+// [1 1 2] target = 4, output = [1 1 2]
+    vector<vector<int> > combinationSum2(vector<int> &num, int target) {
+        sort(num.begin(), num.end());
+        
+        vector<vector<int> > result;
+        vector<int> path;
+        dfs(num, target, 0, path, result);
+        return result;
+    }
+    
+    void dfs(vector<int> &num, int gap, int start, vector<int> &path, vector<vector<int> > &result) {
+        if (gap == 0) {
+            result.push_back(path);
+            return;
+        }
+        
+        int previous = -1;
+        for(int i=start; i<num.size(); ++i) {
+            if (gap < num[i]) return;
+            
+            if (previous == num[i]) continue;
+            
+            previous = num[i];
+            
+            path.push_back(num[i]);
+            dfs(num, gap-num[i], i+1, path, result);
+            path.pop_back();
+        }
+    }
+};
