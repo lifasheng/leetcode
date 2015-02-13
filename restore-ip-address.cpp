2e5b480bcdb8b774f797b830c5f503ece77c191f
fashengli <fashengli@realnetworks.com>
2015-02-13 11:02:49 +0800

    solution for: restore ip address.

---
+++ restore-ip-address.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<string> restoreIpAddresses(string s) {
+        vector<string> result;
+        string path;
+        dfs(s, path, 0, 0, result);
+        return result;
+    }
+    void dfs(string &s, string path, int start, int step, vector<string> &result) {
+        if (start == s.size() && step == 4) {
+            path.resize(path.size()-1);
+            result.push_back(path);
+            return;
+        }
+        
+        //剪枝， 而且通常是第一个剪枝先到达。
+        if (s.size()-start > (4-step)*3) return;
+        if (s.size()-start < (4-step)) return;
+        
+        int num = 0;
+        for(int i=start; i<start+3; ++i) {
+            num = num * 10 + (s[i] - '0');
+            
+            if (num <= 255) {
+                path += s[i];
+                dfs(s, path+'.', i+1, step+1, result);
+            }
+            
+            if (num == 0) break; // 不允许前缀 0,但允许单个 0
+        }
+    }
+};
