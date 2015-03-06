64e386e5ae6e4cda5d9a0ede038029abde8c81ce
fashengli <fashengli@realnetworks.com>
2015-03-06 11:51:01 +0800

    after solving pascals triangle ii, I think it is better to use prev,cur. Because this will make the code more clear.

--- pascals-triangle.cpp
+++ pascals-triangle.cpp
@@ -7,13 +7,13 @@ public:
         result.push_back(vector<int>(1,1)); // 第一行
         // 对第2第numRows行，每一行的头尾是1, 中间元素是上一层的左上角和右上角之和。
         for(int i=2; i<=numRows; ++i) {
-            vector<int> v;
-            v.push_back(1);
+            vector<int> cur(i, 1); // 当前行
+            const vector<int> &prev = result[i-2]; // 上一行
+            
             for(int j=1; j<i-1; ++j) {
-                v.push_back(result[i-2][j-1]+result[i-2][j]);
+                cur[j] = prev[j-1] + prev[j]; // 左上角和右上角之和
             }
-            v.push_back(1);
-            result.push_back(v);
+            result.push_back(cur);
         }
         return result;
     }
