f0bef25ce5ad482dfeabaccce2d4ef3ce1e946ea
fashengli <fashengli@realnetworks.com>
2015-03-10 10:21:00 +0800

    add coment for reverse bits.

--- reverse-bits.cpp
+++ reverse-bits.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
+/*
+思路类似于reverse integer。
+step1：用mask分别判断头尾的bit是0还是1;
+step2: 把头尾的bit清零;
+step3: 对于左边的bit，替换为右边的bit，反之亦然;
+step4: mask往中间移动，直到high_mask < low_mask
+*/
     uint32_t reverseBits(uint32_t n) {
         uint32_t high_mask = 0x80000000;
         uint32_t low_mask  = 0x00000001;
