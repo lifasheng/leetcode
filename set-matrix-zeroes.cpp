680a10c8be478dc4d922bdb84f219333c07b3271
fashengli <fashengli@realnetworks.com>
2015-03-16 17:12:44 +0800

    add comment for: set matrix zeroes.

--- set-matrix-zeroes.cpp
+++ set-matrix-zeroes.cpp
@@ -35,6 +35,17 @@ public:
     }
 #endif
 #ifdef M2 //O(m*n) time, O(1) space, use first row and first col to help
+    /*
+     * 注意：
+     * (1) 这里肯定要单独判断第一行和第一列是否含有0,
+     *     否则如果第一行和第一列没有0,而其它行列有0时，也会将第一行或第一列全部设为0;
+     * (2) 对于把matrix[0][j] = matrix[i][0] = 0这个阶段（代码中的第一个两重循环），i和j可以从0开始，
+     *     不过没必要，因为第一行和第一列已经处理过了。
+     * (3) 对于代码中的第二个两重循环，i和j就不能从0开始，因为如果matrix[0][0] = 0的话，
+     *     在这种情况下会把第一行全部设为0, 从而导致整个数组都全部设为0.
+     *
+     * 所以总结一下就是，先单独判断第一行和第一列是否含有0元素，然后对[1..n-1][1..n-1]进行判断和处理，最后再处理第一行和第一列。
+     */
     void setZeroes(vector<vector<int> > &matrix) {
         const int row = matrix.size();
         const int col = matrix[0].size();
