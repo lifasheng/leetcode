1fb8d95488a92b005574c2b6e6f6002840879172
fashengli <fashengli@realnetworks.com>
2015-03-16 11:50:14 +0800

    rewrite valid sudoku.

--- valid-sudoku.cpp
+++ valid-sudoku.cpp
@@ -43,3 +43,41 @@ public:
         return true;
     }
 };
+
+
+
+class Solution {
+public:
+    bool check(bool isUsed[], char ch) {
+        if (ch == '.') return true;
+        if (isUsed[ch-'1']) return false;
+        return isUsed[ch-'1'] = true;
+    }
+    bool isValidSudoku(vector<vector<char> > &board) {
+        bool isUsed[9];
+        for(int i=0; i<9; ++i) {
+            fill_n(isUsed, 9, false);
+            for(int j=0; j<9; ++j) {
+                if (!check(isUsed, board[i][j])) return false;
+            }
+            
+            fill_n(isUsed, 9, false);
+            for(int j=0; j<9; ++j) {
+                if (!check(isUsed, board[j][i])) return false;
+            }
+        }
+        
+        for(int r=0; r<3; ++r) {
+            for(int c=0; c<3; ++c) {
+                fill_n(isUsed, 9, false);
+                for(int i=0; i<3; ++i) {
+                    for(int j=0; j<3; ++j) {
+                        if (!check(isUsed, board[r*3+i][c*3+j])) return false;
+                    }
+                }
+            }
+        }
+        
+        return true;
+    }
+};
