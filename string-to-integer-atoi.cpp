c94eb2b67c0fcdbc629a71ed8eaeb2bab0784491
fashengli <fashengli@realnetworks.com>
2015-03-18 15:08:49 +0800

    rewrite string to integer atoi.

--- string-to-integer-atoi.cpp
+++ string-to-integer-atoi.cpp
@@ -48,4 +48,49 @@ public:
         
         return (int)result;
     }
+
+    // new implementation.
+    int atoi(string str) {
+        const int n = str.size();
+        if (n==0) return 0;
+        
+        const char *p = &str[0];
+        // handle space
+        while(*p && isspace(*p)) ++p;
+        
+        if(!*p) return 0;
+        
+        int sign = 1;
+        if (*p == '+') {
+            ++p;
+        }
+        else if (*p == '-') {
+            sign=-1; 
+            ++p;
+        }
+        
+        // 这里没有使用long long类型，而是通过和INT_MAX INT_MIN进行特殊判断来处理。
+        int result = 0;
+        while(*p) {
+            if (!isdigit(*p)) break;
+            
+            int d = *p-'0';
+            if (sign==1) {
+                if(result == INT_MAX/10 && d > INT_MAX%10 || result > INT_MAX/10) {
+                    return INT_MAX;
+                }
+            }
+            else {
+                if (result == abs(INT_MIN/10) && d > abs(INT_MIN%10) || -1*result < INT_MIN/10) {
+                    return INT_MIN;
+                }
+            }
+            
+            result = result * 10 + d;
+            
+            ++p;
+        }
+        
+        return sign * result;
+    }
 };
