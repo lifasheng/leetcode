4de0e5f18547f4be69fdedae6b6d3896a3ab31e7
fashengli <fashengli@realnetworks.com>
2015-03-19 11:22:48 +0800

    rewrite longest common prefix.

--- longest-common-prefix.cpp
+++ longest-common-prefix.cpp
@@ -8,11 +8,11 @@ position pos and spans len characters (or until the end of the string, whichever
 */
     string longestCommonPrefix(vector<string> &strs) {
         if (strs.empty()) return "";
-    
-        for(int i=0; i<strs[0].size(); ++i) {
-            for(int j=1; j<strs.size(); ++j) {
-                if (strs[j][i] != strs[0][i]) {
-                    return strs[0].substr(0, i);
+
+        for(int j=0; j<strs[0].size(); ++j) {
+            for(int i=1; i<strs.size(); ++i) {
+                if (strs[i][j] != strs[0][j]) {
+                    return strs[0].substr(0, j);
                 }
             }
         }
