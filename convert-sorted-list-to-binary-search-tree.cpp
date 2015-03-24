c109836917e3238eb5bfe2517972b7c5380df648
fashengli <fashengli@realnetworks.com>
2015-03-24 13:31:43 +0800

    rewrite convert sorted list to binary search tree.

--- convert-sorted-list-to-binary-search-tree.cpp
+++ convert-sorted-list-to-binary-search-tree.cpp
@@ -17,11 +17,13 @@
  */
 class Solution {
 public:
+#define M2
+#ifdef M1  //分治法 O(nlogn)time, O(logn)space
     TreeNode *sortedListToBST(ListNode *head) {
         if (head == NULL) return NULL;
         if (head->next == NULL) return new TreeNode(head->val);
         
-        // fast runner to find middle item (slow points to the middle item).
+        //fast runner to find middle item(pointed by slow)
         ListNode dummy(-1);
         dummy.next = head;
         ListNode *slow = &dummy, *fast = &dummy, *prev = &dummy;
@@ -39,4 +41,32 @@ public:
         root->right = sortedListToBST(head2);
         return root;
     }
+#endif
+#ifdef M2
+// http://articles.leetcode.com/2010/11/convert-sorted-list-to-balanced-binary.html
+    TreeNode *sortedListToBST(ListNode *head) {
+        if (head == NULL) return NULL;
+        if (head->next == NULL) return new TreeNode(head->val);
+        ListNode *p = head;
+        int len = 1;
+        while(p->next) {
+            ++len;
+            p = p->next;
+        }
+        return sortedListToBST(head, 0, len-1);
+    }
+    
+    // 自底向上的方法，这里最巧妙的地方就是list是引用，这样就保证子递归中已经把list正确地往后移动了。
+    TreeNode *sortedListToBST(ListNode* &list, int start, int end) {
+        if (start > end) return NULL;
+        
+        int mid = start + (end-start)/2; // 效果同(start+end)/2, 防止溢出, good
+        TreeNode *leftTree = sortedListToBST(list, start, mid-1);
+        TreeNode *root = new TreeNode(list->val);
+        list = list->next;
+        root->left = leftTree;
+        root->right = sortedListToBST(list, mid+1, end);
+        return root;
+    }
+#endif
 };
