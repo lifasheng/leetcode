3661bc525e7357324bc74fe6e587dd83069f8492
fashengli <fashengli@realnetworks.com>
2015-04-02 16:01:04 +0800

    rewrite unique path ii.

--- unique-paths-ii.cpp
+++ unique-paths-ii.cpp
@@ -31,3 +31,24 @@ public:
         return up + left;
     }
 };
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
+        if(obstacleGrid.empty()) return 0;
+        const int m = obstacleGrid.size();
+        const int n = obstacleGrid[0].size();
+        vector<vector<int> > cache(m+1, vector<int> (n+1, -1));
+        return helper(obstacleGrid, cache, m, n);
+    }
+    
+    int helper(vector<vector<int> > &obstacleGrid, vector<vector<int> > &cache, int m, int n) {
+        if (m<1 || n<1) return 0;
+        if (obstacleGrid[m-1][n-1]) return 0;
+        if (m==1 && n==1) return 1;
+        
+        if (cache[m][n] == -1) {
+            cache[m][n] = helper(obstacleGrid, cache, m-1, n) + helper(obstacleGrid, cache, m, n-1);
+        }
+        return cache[m][n];
+    }
+};
