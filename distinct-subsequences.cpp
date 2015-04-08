5bbc101e307e373637b5659a4fd3905b561386c9
fashengli <fashengli@realnetworks.com>
2015-04-08 16:44:32 +0800

    rewrite distinct subsequences.

--- distinct-subsequences.cpp
+++ distinct-subsequences.cpp
@@ -87,3 +87,27 @@ f(i, j) = f(i-1, j) + f(i-1, j-1);
     }
 #endif
 };
+// f[i][j]表示S[0-i]和T[0-j]的匹配子串的个数
+class Solution {
+public:
+    int numDistinct(string S, string T) {
+        const int m = S.size();
+        const int n = T.size();
+        if (m<n) return 0;
+        vector<vector<int> > f(m+1, vector<int>(n+1));
+        
+        for(int j=0; j<=n; ++j) f[0][j] = 0;
+        for(int i=0; i<=m; ++i) f[i][0] = 1;
+        
+        for(int i=1; i<=m; ++i) {
+            for(int j=1; j<=n; ++j) {
+                if (i>=j) {
+                    f[i][j] = f[i-1][j];
+                    if (S[i-1] == T[j-1]) f[i][j] += f[i-1][j-1];
+                }
+            }
+        }
+        
+        return f[m][n];
+    }
+};
