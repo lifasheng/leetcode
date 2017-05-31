b59d91c87b86c54ebf172b3d0c15098139bd71c3
Fasheng Li <fashengl@amazon.com>
2017-05-31 13:57:34 -0700

    382. Linked List Random Node - Reservoir sampling

--- linked-list-random-node.cpp
+++ linked-list-random-node.cpp
@@ -53,3 +53,48 @@ public:
  * Solution obj = new Solution(head);
  * int param_1 = obj.getRandom();
  */
+
+
+using "Reservoir sampling" O(1) space, O(n) time complexity，c++
+according to the wiki https://en.wikipedia.org/wiki/Reservoir_sampling
+here is sudo code for k size reservoir:
+
+/*
+  S has items to sample, R will contain the result
+*/
+ReservoirSample(S[1..n], R[1..k])
+  // fill the reservoir array
+  for i = 1 to k
+      R[i] := S[i]
+
+  // replace elements with gradually decreasing probability
+  for i = k+1 to n
+    j := random(1, i)   // important: inclusive range
+    if j <= k
+        R[j] := S[i]
+you need to remember the range [ 0, i ] should be inclusive.
+
+class Solution {
+private:
+    ListNode* head;
+public:
+    /** @param head The linked list's head. Note that the head is guanranteed to be not null, so it contains at least one node. */
+    Solution(ListNode* head) {
+        this->head = head;
+    }
+    
+    /** Returns a random node's value. */
+    int getRandom() {
+        int res = head->val;
+        ListNode* node = head->next;
+        int i = 2;
+        while(node){
+            int j = rand()%i;
+            if(j==0)
+                res = node->val;
+            i++;
+            node = node->next;
+        }
+        return res;
+    }
+};
