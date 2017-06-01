a2a1917b0ad4cecc2aec6ace0a3708acc63742ab
Fasheng Li <fashengl@amazon.com>
2017-06-01 10:18:35 -0700

    386. Lexicographical Numbers

---
+++ lexicographical-numbers.cpp
@@ -0,0 +1,83 @@
+/*
+Given an integer n, return 1 - n in lexicographical order.
+For example, given 13, return: [1,10,11,12,13,2,3,4,5,6,7,8,9].
+Please optimize your algorithm to use less time and space. The input size may be as large as 5,000,000.
+*/
+
+
+// my solution
+/*
+1      2     3   ... 9
+10    11   12  13 .. 19 20 ... 29
+100 101 ...109    110..119   120..129 130.. 139
+
+we can see that it is a recursive process.
+*/
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> result;
+        for (int i=1; i<=9; ++i) {
+            helper(n, i, result);
+        }
+        return result;
+    }
+    
+    void helper(int n, int i, vector<int> &vi) {
+        if (i>n) return;
+        vi.push_back(i);
+        for (int j=i*10; j<(i+1)*10; ++j) {
+            helper(n, j, vi);
+        }
+    }
+};
+
+
+// loop solution
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> res(n);
+        int cur = 1;
+        for (int i = 0; i < n; i++) {
+            res[i] = cur;
+            if (cur * 10 <= n) {
+                cur *= 10;
+            } else {
+                if (cur >= n) 
+                    cur /= 10;
+                cur += 1;
+                while (cur % 10 == 0)
+                    cur /= 10;
+            }
+        }
+        return res;
+    }
+};
+
+
+// Java solution
+public List<Integer> lexicalOrder(int n) {
+        List<Integer> list = new ArrayList<>(n);
+        int curr = 1;
+        for (int i = 1; i <= n; i++) {
+            list.add(curr);
+            if (curr * 10 <= n) {
+                curr *= 10;
+            } else if (curr % 10 != 9 && curr + 1 <= n) {
+                curr++;
+            } else {
+                while ((curr / 10) % 10 == 9) {
+                    curr /= 10;
+                }
+                curr = curr / 10 + 1;
+            }
+        }
+        return list;
+    }
+/*
+The basic idea is to find the next number to add.
+Take 45 for example: if the current number is 45, the next one will be 450 (450 == 45 * 10)(if 450 <= n), or 46 (46 == 45 + 1) (if 46 <= n) or 5 (5 == 45 / 10 + 1)(5 is less than 45 so it is for sure less than n).
+We should also consider n = 600, and the current number = 499, the next number is 5 because there are all "9"s after "4" in "499" so we should divide 499 by 10 until the last digit is not "9".
+It is like a tree, and we are easy to get a sibling, a left most child and the parent of any node.
+*/
