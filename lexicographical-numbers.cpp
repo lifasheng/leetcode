6d8af5fc4213d421dd9297271901d1d1f06c6a50
Fasheng Li <fashengl@amazon.com>
2017-06-01 19:10:05 -0700

    386. Lexicographical Numbers

--- lexicographical-numbers.cpp
+++ lexicographical-numbers.cpp
@@ -7,11 +7,16 @@ Please optimize your algorithm to use less time and space. The input size may be
 
 // my solution
 /*
+n = 245
+1   10 100 101 102 103 ... 109   11 110 111 112 ... 119   12 120 121 ... 129   13 130 131 ... 139     14 140 141 ... 149     15 150 151 ... 159     .......   19 190 191 ... 199
+2   20 200 201 202 203 ... 209   21 ........
+
 1      2     3   ... 9
 10    11   12  13 .. 19 20 ... 29
 100 101 ...109    110..119   120..129 130.. 139
 
-we can see that it is a recursive process.
+we can see that it is a depth first search issue.
+we can solve it using recursive process.
 */
 class Solution {
 public:
@@ -26,12 +31,20 @@ public:
     void helper(int n, int i, vector<int> &vi) {
         if (i>n) return;
         vi.push_back(i);
-        for (int j=i*10; j<(i+1)*10; ++j) {
+        for (int j=i*10; j<=n && j<(i+1)*10; ++j) {
             helper(n, j, vi);
         }
     }
 };
 
+
+
+/*
+The basic idea is to find the next number to add.
+Take 45 for example: if the current number is 45, the next one will be 450 (450 == 45 * 10)(if 450 <= n), or 46 (46 == 45 + 1) (if 46 <= n) or 5 (5 == 45 / 10 + 1)(5 is less than 45 so it is for sure less than n).
+We should also consider n = 600, and the current number = 499, the next number is 5 because there are all "9"s after "4" in "499" so we should divide 499 by 10 until the last digit is not "9".
+It is like a tree, and we are easy to get a sibling, a left most child and the parent of any node.
+*/
 
 // loop solution
 class Solution {
@@ -44,10 +57,10 @@ public:
             if (cur * 10 <= n) {
                 cur *= 10;
             } else {
-                if (cur >= n) 
+                if (cur >= n)  //  = is important here! for example, n = 15, when cur == 15, we should move to 2 . acutally we can use (cur == n) here.
                     cur /= 10;
                 cur += 1;
-                while (cur % 10 == 0)
+                while (cur % 10 == 0) // for example, n = 215, when cur = 199, we should move to 2
                     cur /= 10;
             }
         }
@@ -75,9 +88,3 @@ public List<Integer> lexicalOrder(int n) {
         }
         return list;
     }
-/*
-The basic idea is to find the next number to add.
-Take 45 for example: if the current number is 45, the next one will be 450 (450 == 45 * 10)(if 450 <= n), or 46 (46 == 45 + 1) (if 46 <= n) or 5 (5 == 45 / 10 + 1)(5 is less than 45 so it is for sure less than n).
-We should also consider n = 600, and the current number = 499, the next number is 5 because there are all "9"s after "4" in "499" so we should divide 499 by 10 until the last digit is not "9".
-It is like a tree, and we are easy to get a sibling, a left most child and the parent of any node.
-*/
