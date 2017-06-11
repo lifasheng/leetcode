507a0a87d7b41b6d2aeef51725dd7f9f66477d21
Fasheng Li <fashengl@amazon.com>
2017-06-10 17:17:43 -0700

    392. Is Subsequence

--- is-subsequence.cpp
+++ is-subsequence.cpp
@@ -46,6 +46,31 @@ public:
 统计t中每个字符出现的位置。
 对于s中的字符，判断它是否出现在t中，以及它的位置是否在t对应字符的位置范围中，在的话，记录最小的位置。
 显然，这里判断位置是否在一个范围中很容易，但要找到最小的合适位置就要用到二分查找。
+
+这种解法适合t不变，而s1,s2,...sk不停来查询的情况。
+
+ i.e:
+
+t=abacddef
+  01234567
+s=adf
+  012
+
+m[a]=[0,2]
+m[b]=[1]
+m[c]=[3]
+m[d]=[4,5]
+m[e]=[6]
+m[f]=[7]
+
+a: pos=-1<2, pos更新为0
+d: pos=0<5,  pos更新为4
+f: pos=4<7,  pos更新为7
+
+如果s=afe
+a: pos=-1<2, pos更新为0
+f: pos=0<7,  pos更新为7
+e: pos=7>6,  return false
 */
 #if 1
 class Solution {
@@ -71,6 +96,45 @@ public:
                         break;
                     }
                 }
+            }
+        }
+
+        return true;
+    }
+};
+#endif
+
+
+
+// similar with method 2, 只是用二分搜索的方式来找。
+#if 0
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        unordered_map<char, vector<int> > m;
+        int pos = 0;
+        for (auto c : t) {
+            m[c].push_back(pos++);
+        }
+
+        pos = -1;
+        for (auto c : s) {
+            if (m.find(c) == m.end()) return false;
+            vector<int> vi = m[c];
+            if (vi.empty()) return false;
+            if (vi[vi.size()-1] <= pos) {
+                return false;
+            } else {
+                /*
+                for (int i=0; i<vi.size(); ++i) {
+                    if (vi[i] > pos) {
+                        pos = vi[i];
+                        break;
+                    }
+                }
+                */
+                vector<int>::iterator iter = upper_bound(vi.begin(), vi.end(), pos);
+                pos = *iter;
             }
         }
 
