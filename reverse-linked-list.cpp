8aa440d0f6cc9deaa88b4c228c1e7f3c980a05f3
Fasheng Li <fashengl@amazon.com>
2019-03-07 17:12:42 -0800

    206. Reverse Linked List - recursive implementation, very interesting

--- reverse-linked-list.cpp
+++ reverse-linked-list.cpp
@@ -15,7 +15,6 @@ A linked list can be reversed either iteratively or recursively. Could you imple
 
 */
 
-
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -26,6 +25,7 @@ A linked list can be reversed either iteratively or recursively. Could you imple
  */
 class Solution {
 public:
+    #if 0
     ListNode* reverseList(ListNode* head) {        
         ListNode* head2 = NULL;
         ListNode* p = NULL;
@@ -38,4 +38,58 @@ public:
         
         return head2;
     }
+    
+    #endif
+    
+    #if 1
+    /*
+    Try to record the new head and new tail.
+    New head will be updated once, new tail will be updated every time the sub call returned.
+    */
+    ListNode* reverseList(ListNode* head) {        
+        if (head == NULL) return head;
+        ListNode *newHead = NULL;
+        ListNode *newTail = NULL;
+        reverseList(head, newHead, newTail);
+        return newHead;
+    }
+    
+    void reverseList(ListNode* head, ListNode* &newHead, ListNode* &newTail) {        
+        if (head->next == NULL) {
+            newHead = newTail = head;
+            return;
+        }
+        
+        ListNode *p = head->next;
+        head->next = NULL;
+        reverseList(p, newHead, newTail);
+        newTail->next = head;
+        newTail = head;
+    }
+    #endif
+    
+    #if 0
+    /*
+    The recursive version is slightly trickier and the key is to work backwards. Assume that the rest of the list had already been reversed, now how do I reverse the front part? Let's assume the list is: n1 → … → nk-1 → nk → nk+1 → … → nm → Ø
+
+    Assume from node nk+1 to nm had been reversed and you are at node nk.
+
+    n1 → … → nk-1 → nk → nk+1 ← … ← nm
+
+    We want nk+1’s next node to point to nk.
+
+    So,
+
+    nk.next.next = nk;
+
+    Be very careful that n1's next must point to Ø. If you forget about this, your linked list has a cycle in it. This bug could be caught if you test your code with a linked list of size 2.
+    */
+    ListNode* reverseList(ListNode* head) {       
+        if (head == NULL || head->next == NULL) return head;
+        ListNode* p = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return p;
+    }
+    #endif
 };
