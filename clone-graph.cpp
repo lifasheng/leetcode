6ebf3984d3ae34393f04376a84a8a1915d65c3c3
Fasheng Li <fashengl@amazon.com>
2019-06-28 16:16:16 +0800

    update bfs method for clone graph

--- clone-graph.cpp
+++ clone-graph.cpp
@@ -27,41 +27,6 @@ public:
     }
 };
 
-// bfs
-// 对于在map中存在的结点，它肯定已经被访问过了，所以不用再加入队列了。
-// 而在队列中的结点，肯定是已经clone过了，所以只需要处理它的邻居就行了。
-class Solution {
-public:
-    UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
-        if (!node) return node;
-        
-        unordered_map<UndirectedGraphNode*, UndirectedGraphNode*> m;
-        queue<UndirectedGraphNode*> q;
-        q.push(node);
-        m[node] = new UndirectedGraphNode(node->label);
-        while(!q.empty()) {
-            UndirectedGraphNode *n = q.front();
-            q.pop();
-            
-            for(auto nbr: n->neighbors) {
-                if (m.find(nbr) != m.end()) {
-                    m[n]->neighbors.push_back(m[nbr]);
-                }
-                else {
-                    m[nbr] = new UndirectedGraphNode(nbr->label);
-                    m[n]->neighbors.push_back(m[nbr]);
-                    q.push(nbr);
-                }
-            }
-        }
-        
-        return m[node];
-    }
-};
-
-
-
-
 
 
 /*
@@ -143,5 +108,39 @@ public:
                 m[node]->neighbors.push_back(m[neighbor]);
             }
         }
+    }
+};
+
+
+
+// bfs
+// 我们需要保证clone后的邻居顺序也不能变，比如1：2-4, 2: 3-1, 4: 3-1, 3: 2-4
+// 当然我们也可以像上面的方法一下，先bfs找到所有的节点，并clone，然后connect cloned的节点。但这样每个节点要访问两次。
+// 我们只能在访问邻居的时候，如果该邻居还没有被clone，就当时clone它，这样才能保证新的节点的neighbors数组中的邻居顺序。
+// 队列里只保存已经clone过的节点，我们只需要处理它的邻居就行了。
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if (node == NULL) return NULL;
+
+        unordered_map<Node*, Node*> m;
+        queue<Node*> q;
+
+        m[node] = new Node(node->val);
+        q.push(node);
+        while(!q.empty()) {
+            Node *n = q.front();
+            q.pop();
+
+            for(auto nbr: n->neighbors) {
+                if (m.find(nbr) == m.end()) {
+                    m[nbr] = new Node(nbr->val);
+                    q.push(nbr);
+                }
+                m[n]->neighbors.push_back(m[nbr]);
+            }
+}
+
+        return m[node];
     }
 };
