77aec7870c61f9f47ab7439771f3cc4888250776
Fasheng Li <fashengl@amazon.com>
2020-12-11 11:56:16 -0800

    86. Partition List

---
+++ java-versions/partition-list.java
@@ -0,0 +1,46 @@
+/*
+86. Partition List
+Medium
+Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+You should preserve the original relative order of the nodes in each of the two partitions.
+
+Example:
+
+Input: head = 1->4->3->2->5->2, x = 3
+Output: 1->2->2->4->3->5
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode dummyHead1 = new ListNode(-1);
+        ListNode dummyHead2 = new ListNode(-1);
+        ListNode p1 = dummyHead1;
+        ListNode p2 = dummyHead2;
+        ListNode p = head;
+        while(p!=null) {
+            if (p.val < x) {
+                p1.next = p;
+                p1 = p1.next;
+            } else {
+                p2.next = p;
+                p2 = p2.next;
+            }
+            p = p.next;
+        }
+        p1.next = dummyHead2.next;
+        p2.next = null;
+        return dummyHead1.next;
+    }
+}
+
