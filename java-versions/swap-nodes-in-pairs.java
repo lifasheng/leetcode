f20eeeb961ab6a124cbc250f48850fe545c3171b
Fasheng Li <fashengl@amazon.com>
2020-12-12 21:49:17 -0800

    add recusive way for swap nodes in pairs

--- java-versions/swap-nodes-in-pairs.java
+++ java-versions/swap-nodes-in-pairs.java
@@ -30,15 +30,18 @@ Output: [1]
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-/*
-思路：
-用prev指针指向cur的前一个结点，cur和next指向当前结点和下一个结点。
-每次循环，可以翻转cur和next，然后更新prev指针和cur指针
-O(n) time, O(1) space
-Test case: 1->2,  1->2->3, 1->2->3->4, 1->2->3->4->5
-*/
+
+
 class Solution {
-    public ListNode swapPairs(ListNode head) {
+    /*
+    迭代版
+    思路：
+    用prev指针指向cur的前一个结点，cur和next指向当前结点和下一个结点。
+    每次循环，可以翻转cur和next，然后更新prev指针和cur指针
+    O(n) time, O(1) space
+    Test case: 1->2,  1->2->3, 1->2->3->4, 1->2->3->4->5
+    */
+    public ListNode swapPairs1(ListNode head) {
         if (head == null || head.next == null) return head;
         
         ListNode dummyHead = new ListNode(-1, head);
@@ -55,6 +58,26 @@ class Solution {
             curr = curr.next;
         }
         return dummyHead.next;
+    }
+    
+    /*
+    递归版: Time: O(N), Space: O(N)
+    很直观！
+    */
+    public ListNode swapPairs2(ListNode head) {
+        if (head == null || head.next == null) return head;
+        
+        ListNode p = head;
+        ListNode q = p.next;
+        ListNode remain = swapPairs2(q.next);
+        
+        q.next = p;
+        p.next = remain;
+        return q;
+    }
+    
+    public ListNode swapPairs(ListNode head) {
+        return swapPairs2(head);
     }
 }
 
